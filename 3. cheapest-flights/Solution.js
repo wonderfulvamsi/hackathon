cd192654324dfd2bf9593e83cdd82e0b88d106b5
Sai Vamsi Yelamanchili <saivamsiyelamanchili@Sais-MacBook-Pro.local>
2024-03-02 12:01:59 +0530

    tym up

--- 3. cheapest-flights/Solution.js
+++ 3. cheapest-flights/Solution.js
@@ -1,5 +1,40 @@
 function findCheapestPrice(n, flights, src, dst, k) {
-    // add your solution here
+    const toAdjList = (arr)=>{
+        let g = {}
+        for(let i =0;i<arr.length;i++){
+            u = arr[i][0]
+            v = arr[i][1]
+            c = arr[i][2]
+            if(g[u] && g[u].length){
+                g[u].push([v,c])
+            }
+            else{
+                g[u] = []
+                g[u].push([v,c])
+            }
+        }
+        return g
+    }
+
+    let g = toAdjList(flights)
+    console.log(g)
+
+    const dfs = (src, dst, k, cost=0)=>{
+        //base case
+        if(src == dst){
+            return cost
+        }
+        if(k==0){
+            i=0
+            while(i<g[src].length){
+                if(g[src][i][0] == dst){
+                    return g[src][i][2]
+                }
+                i+=1
+            }
+        }
+    }
+    return -1
 }
 
 // Test case 1
@@ -12,4 +47,4 @@ console.log("Actual: " + findCheapestPrice(4, flights2, 0, 3, 0) + ", Expected: 
 
 // Test case 3
 const flights3 = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]];
-console.log("Actual: " + findCheapestPrice(3, flights3, 0, 2, 0) + ", Expected: 500");
+console.log("Actual: " + findCheapestPrice(3, flights3, 0, 2, 0) + ", Expected: -1");
